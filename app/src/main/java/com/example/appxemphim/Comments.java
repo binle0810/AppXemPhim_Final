70f3900afb9da8ed1dc7d9a891ab55f1baf5e23f
binle <binlephuoc0810@gmail.com>
2023-11-09 17:07:56 +0700

    first commit

---
+++ app/src/main/java/com/example/appxemphim/Comments.java
@@ -0,0 +1,15 @@
+package com.example.appxemphim;
+
+public class Comments {
+    public String userID, comment, time;
+
+    public Comments(){
+
+    }
+
+    public Comments(String userID, String comment, String time) {
+        this.userID = userID;
+        this.comment = comment;
+        this.time = time;
+    }
+}
